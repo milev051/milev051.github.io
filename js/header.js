641dc5239753cc7e48986c871e728e6bc41f6d2a
milev051 <milev051@gmail.com>
2022-08-11 02:24:48 +0200

    20220811_022447

--- js/header.js
+++ js/header.js
@@ -9,7 +9,6 @@ document.body.insertAdjacentHTML(
         <p class='header-logo-holder' href='/'><img src='img/logo/logo black.png' alt='' class='header-logo'></p>\
         <nav>\
             <a class='nav-element' href='gallery'>gallery</a>\
-            <a class='nav-element' href='https://www.instagram.com/milev.rs/'>instagram</a>\
             <a class='nav-element' href='about'>about</a>\
         </nav>\
     </header>\
@@ -18,4 +17,20 @@ document.body.insertAdjacentHTML(
 // document.body.insertAdjacentHTML(
 //     "beforeend",
 //     "<hr>"
+// )
+
+
+// OLD CODE
+// document.body.insertAdjacentHTML(
+//     "afterbegin",
+//     "\
+//     <header>\
+//         <p class='header-logo-holder' href='/'><img src='img/logo/logo black.png' alt='' class='header-logo'></p>\
+//         <nav>\
+//             <a class='nav-element' href='gallery'>gallery</a>\
+//             <a class='nav-element' href='https://www.instagram.com/milev.rs/'>instagram</a>\
+//             <a class='nav-element' href='about'>about</a>\
+//         </nav>\
+//     </header>\
+//   "
 // )
