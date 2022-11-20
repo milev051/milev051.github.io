fdf222b31a067d2167d446aff5a425ed7df02e12
Marko Milev <milev051@gmail.com>
2022-11-20 15:02:21 +0100

    20221120_145841

--- EPOS/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  playMusic();
-});
-
-function playMusic() {
-      var audio = new Audio("../media/song-01.mp3");
-      audio.play();
-      audio.loop = true;
-  }
