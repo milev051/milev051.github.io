8c654173483af968f2f3fcd9ff16bcd3c5bbfd26
milev051 <milev051@gmail.com>
2022-12-13 01:53:51 +0100

    20221213_015349

--- js/head.js
+++ js/head.js
@@ -5,13 +5,13 @@ document.head.insertAdjacentHTML(
     <meta name='viewport' content='width-device-width, initial-scale=1.0'>
     <meta name='author' content='milev'></meta>
     <link rel='shortcut icon' type='image/x-icon' href='img/logo/logo white.png' />
-    <title>`+ window.location.pathname.split("/").pop().charAt(0).toUpperCase() + window.location.pathname.split("/").pop().slice(1) + `</title>
+    <title>`+ window.location.pathname.split('/').pop().charAt(0).toUpperCase() + window.location.pathname.split('/').pop().slice(1) + `</title>
     
     <link rel='stylesheet' href='css/@animations.css'></link>
     <link rel='stylesheet' href='css/@general.css'>
     <link rel='stylesheet' href='css/@header.css'>
     <link rel='stylesheet' href='css/@text.css'>
-    <link rel='stylesheet' href='css/`+ window.location.pathname.split("/").pop() + `.css'>
+    <link rel='stylesheet' href='css/`+ window.location.pathname.split('/').pop() + `.css'>
     
     <link rel='preconnect' href='https://fonts.googleapis.com'>
     <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
